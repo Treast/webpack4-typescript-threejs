c141cd2538d2bd9df7f094ce1455537da5907400
Vincent Riva <vincent@wearewanaka.com>
2019-12-13 13:14:56 +0100

    Update onResize

--- src/app/js/Scene.ts
+++ src/app/js/Scene.ts
@@ -27,6 +27,8 @@ class Scene {
   }
 
   onResize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
     this.renderer.setSize(window.innerWidth, window.innerHeight);
   }
 
